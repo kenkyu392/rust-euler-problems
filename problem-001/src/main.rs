2902f0ed9a7b27e436b21dfc2a4bd30a517f3d7f
kenkyu392 <57144941+kenkyu392@users.noreply.github.com>
2019-11-30 03:01:29 +0900

    Add

---
+++ problem-001/src/main.rs
@@ -0,0 +1,14 @@
+// Problem 1: Multiples of 3 and 5
+// If we list all the natural numbers below 10 that are multiples of
+// 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Find the sum of all the multiples of 3 or 5 below 1000.
+fn main() {
+    let want = 233168;
+    let mut got = 0;
+    for i in 0..1000 {
+        if i % 3 == 0 || i % 5 == 0 {
+            got += i;
+        }
+    }
+    println!("want:{} == got:{} = {}", want, got, want == got)
+}
